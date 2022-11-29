2c579434d7b73975fb0dc3d587b9dd72b7fff9c2
pulok-thedeveloper <mdpulokhasan@gmail.com>
2022-11-29 22:49:14 +0600

    Dashboard drawer updated

--- src/Layout/Dashboard.js
+++ src/Layout/Dashboard.js
@@ -2,11 +2,13 @@ import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { AuthContext } from '../context/AuthProvider';
 import useAdmin from '../hooks/useAdmin';
+import useSeller from '../hooks/useSeller';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
 const Dashboard = () => {
     const { user } = useContext(AuthContext);
-    const [isAdmin] = useAdmin(user?.email)
+    const [isAdmin] = useAdmin(user?.email);
+    const [isSeller] = useSeller(user?.email);
     return (
         <div>
             <Navbar></Navbar>
@@ -18,9 +20,17 @@ const Dashboard = () => {
                 <div className="drawer-side">
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 text-base-content">
-                        <li><Link to="/dashboard">My Orders</Link></li>
-                        <li><Link to="/dashboard/addproduct">Add Product</Link></li>
-                        <li><Link to="/dashboard/myproducts">My Products</Link></li>
+                        {
+                            user?.role === 'buyer' && 
+                            <li><Link to="/dashboard">My Orders</Link></li>
+                        }
+                        {
+                            isSeller && <>
+                                <li><Link to="/dashboard/addproduct">Add Product</Link></li>
+                                <li><Link to="/dashboard/myproducts">My Products</Link></li>
+                            </>
+                        }
+
                         {
                             isAdmin && <>
                                 <li><Link to="/dashboard/allsellers">All Sellers</Link></li>
